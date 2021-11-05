74b09aa3ae0dd8d83fb8635c7287ea401a926c9b
Alexandre Plateau <lexplt.dev@gmail.com>
2021-11-05 11:19:44 +0100

    adding express and dotenv

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-const express = require('express');
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello world!')
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`App started at http://localhost:${process.env.PORT}`)
+})
