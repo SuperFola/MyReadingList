44fec459fb6237ed9786b099364f49b7d3056e07
Alexandre Plateau <lexplt.dev@gmail.com>
2021-11-10 12:49:29 +0100

    moving db related files under /db

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express()
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'jade')
 
-app.set('db', require('./db_adapter'))
+app.set('db', require('./db/db_adapter'))
 
 app.use(logger('dev'))
 app.use(express.json())
