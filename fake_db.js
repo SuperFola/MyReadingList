0231a43bd0424a8fbc2fae9d04497c7c71d5a62f
Alexandre Plateau <lexplt.dev@gmail.com>
2021-11-10 11:32:06 +0100

    tags are strings only now

--- fake_db.js
+++ fake_db.js
@@ -1,12 +1,10 @@
 module.exports = {
     tags: [
         {
-            id: 0xdeadbeef,
             name: "bar",
             color: 0xff00ff,
         },
         {
-            id: 0xc0ffee,
             name: "coffee",
             color: 0x00ff00,
         },
@@ -15,7 +13,7 @@ module.exports = {
         {
             id: 0,
             title: "Foo",
-            tags: [0xdeadbeef, 0xc0ffee],
+            tags: ["bar", "coffee"],
             read: false,
             url: "https://google.com",
             notes: "fake note",
@@ -25,7 +23,7 @@ module.exports = {
         {
             id: 1,
             title: "Egg",
-            tags: [0xc0ffee],
+            tags: ["coffee"],
             read: false,
             url: "https://dev.to",
             notes: "a nice note",
